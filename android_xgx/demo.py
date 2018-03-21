d318e3e54904f731b4bff583ecd5ea3c36468933
root <root@localhost.localdomain>
2018-03-21 15:06:58 +0800

    add app auto test demo file

---
+++ android_xgx/demo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding=utf-8 -*-
+
+import time
+import sys
+from appium import webdriver
+
+def login(name,passwd):
+    desired_caps = {}
+    desired_caps['platformName'] = "Android"
+    desired_caps['platformVersion'] = "4.4.4"
+    desired_caps['deviceName'] = "MI3"
+    desired_caps['appPackage'] = "com.wwkj.xueguoxue"
+    desired_caps['appActivity'] = ".view.SplashActivity"
+    dd = webdriver.Remote("http://172.16.20.202:4723/wd/hub", desired_caps)
+    time.sleep(6)
+    try:
+        dd.find_element_by_id('login_phone_input').send_keys(name)
+        dd.find_element_by_id("login_password_input").send_keys(passwd)
+        dd.find_element_by_id('login_button').click()
+        time.sleep(3)
+    except:
+        print "error or login over"
+    finally:
+        dd.quit()
+
+if __name__ == "__main__":
+    n = "13141032576"
+    p = "lihailong123"
+    login(n,p)
