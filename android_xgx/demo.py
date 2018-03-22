18746564b4b5af2205ced28e71444cac493be6e9
root <root@localhost.localdomain>
2018-03-22 17:22:39 +0800

    modify file

--- android_xgx/demo.py
+++ android_xgx/demo.py
@@ -5,35 +5,57 @@ import time
 import sys
 from appium import webdriver
 
-def login(name,passwd):
-    desired_caps = {}
-    desired_caps['platformName'] = "Android"
-    desired_caps['platformVersion'] = "4.4.4"
-    desired_caps['deviceName'] = "MI3"
-    desired_caps['appPackage'] = "com.wwkj.xueguoxue"
-    desired_caps['appActivity'] = ".view.MainActivity"
-    dd = webdriver.Remote("http://172.16.20.202:4723/wd/hub", desired_caps)
-    time.sleep(6)
-    try:
-        dd.find_element_by_id('index_login').click()
-        time.sleep(3)
-    except:
-        print "error or login over"
-        dd.quit()
-    finally:
-        pass
-    dd.find_element_by_id('login_phone_input').send_keys(name)
-    dd.find_element_by_id("login_password_input").send_keys(passwd)
-    dd.find_element_by_id('login_button').click()
+class apptest():
+    def __init__(self):
+        self.dd = self.MO()
 
-def menu():
-    dd.find_element_by_id(u'首页').click()
-    dd.find_element_by_id(u'选课').click()
-    dd.find_element_by_id(u'学习').click()
-    dd.find_element_by_id(u'消息').click()
-    dd.find_element_by_id(u'我的').click()
+    def MO(self):
+        self.desired_caps = {}
+        self.desired_caps['platformName'] = "Android"
+        self.desired_caps['platformVersion'] = "4.4.4"
+        self.desired_caps['deviceName'] = "MI3"
+        self.desired_caps['appPackage'] = "com.wwkj.xueguoxue"
+        self.desired_caps['appActivity'] = ".view.MainActivity"
+        self.dd = webdriver.Remote("http://172.16.20.202:4723/wd/hub", self.desired_caps)
+        return self.dd
+
+    def login(self,name,passwd):
+        try:
+            self.dd.find_element_by_id('index_login').click()
+            time.sleep(3)
+        except:
+            print "error or login over"
+            print "progarm is exit"
+            self.dd.quit()
+            sys.exit()
+        finally:
+            pass
+        self.dd.find_element_by_id('login_phone_input').send_keys(name)
+        self.dd.find_element_by_id("login_password_input").send_keys(passwd)
+        self.dd.find_element_by_id('login_button').click()
+        time.sleep(3)
+
+    def menu(self):
+        self.dd.find_element_by_name(u'首页').click()
+        time.sleep(2)
+        self.dd.find_element_by_name(u'选课').click()
+        self.dd.find_element_by_name(u'学习').click()
+        self.dd.find_element_by_name(u'消息').click()
+        self.dd.find_element_by_name(u'我的').click()
+        self.dd.find_element_by_id('logout_button').click()
+
+    def logout(self):
+        self.dd.find_element_by_name(u'我的').click()
+        print "click logout"
+        #self.dd.find_element_by_name('').click()
+        #self.dd.find_element_by_id('logout_button').click()
+
+    def __del__(self):
+        self.logout()
 
 if __name__ == "__main__":
     n = "13141032576"
     p = "lihailong123"
-    login(n,p)
+    test = apptest()
+    #test.login(n,p)
+    test.menu()
