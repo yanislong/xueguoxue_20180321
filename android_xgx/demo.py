e8ef12dbc5efe4c314646478329116a49c8b1c7d
root <root@localhost.localdomain>
2018-03-22 16:12:08 +0800

    modify file

--- android_xgx/demo.py
+++ android_xgx/demo.py
@@ -11,18 +11,20 @@ def login(name,passwd):
     desired_caps['platformVersion'] = "4.4.4"
     desired_caps['deviceName'] = "MI3"
     desired_caps['appPackage'] = "com.wwkj.xueguoxue"
-    desired_caps['appActivity'] = ".view.SplashActivity"
+    desired_caps['appActivity'] = ".view.MainActivity"
     dd = webdriver.Remote("http://172.16.20.202:4723/wd/hub", desired_caps)
     time.sleep(6)
     try:
-        dd.find_element_by_id('login_phone_input').send_keys(name)
-        dd.find_element_by_id("login_password_input").send_keys(passwd)
-        dd.find_element_by_id('login_button').click()
+        dd.find_element_by_id('index_login').click()
         time.sleep(3)
     except:
         print "error or login over"
-    finally:
         dd.quit()
+    finally:
+        pass
+    dd.find_element_by_id('login_phone_input').send_keys(name)
+    dd.find_element_by_id("login_password_input").send_keys(passwd)
+    dd.find_element_by_id('login_button').click()
 
 if __name__ == "__main__":
     n = "13141032576"
