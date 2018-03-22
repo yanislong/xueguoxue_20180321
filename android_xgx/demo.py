ea62354e2b09d7c66bae7b693257aa7e3decb661
yanislong <335916781@qq.com>
2018-03-22 09:32:55 +0000

    modify
    123

--- android_xgx/demo.py
+++ android_xgx/demo.py
@@ -5,6 +5,50 @@ import time
 import sys
 from appium import webdriver
 
+#获得机器屏幕大小x,y
+def getSize():
+    x = dr.get_window_size()['width']
+    y = dr.get_window_size()['height']
+    return (x, y)
+ 
+#屏幕向上滑动
+def swipeUp(t):
+    l = getSize()
+    x1 = int(l[0] * 0.5)  #x坐标
+    y1 = int(l[1] * 0.75)   #起始y坐标
+    y2 = int(l[1] * 0.25)   #终点y坐标
+    dr.swipe(x1, y1, x1, y2,t)
+#屏幕向下滑动
+def swipeDown(t):
+    l = getSize()
+    x1 = int(l[0] * 0.5)  #x坐标
+    y1 = int(l[1] * 0.25)   #起始y坐标
+    y2 = int(l[1] * 0.75)   #终点y坐标
+    dr.swipe(x1, y1, x1, y2,t)
+#屏幕向左滑动
+def swipLeft(t):
+    l=getSize()
+    x1=int(l[0]*0.75)
+    y1=int(l[1]*0.5)
+    x2=int(l[0]*0.05)
+    dr.swipe(x1,y1,x2,y1,t)
+#屏幕向右滑动
+def swipRight(t):
+    l=getSize()
+    x1=int(l[0]*0.05)
+    y1=int(l[1]*0.5)
+    x2=int(l[0]*0.75)
+    dr.swipe(x1,y1,x2,y1,t)
+#调用向左滑动
+swipLeft(1000)
+sleep(3)
+#调用向右滑动
+swipRight(1000)
+调用向上滑动
+swipeUp(1000)
+调用向下滑动
+swipeDown(1000)
+
 class apptest():
     def __init__(self):
         self.dd = self.MO()
@@ -42,6 +86,7 @@ class apptest():
         self.dd.find_element_by_name(u'学习').click()
         self.dd.find_element_by_name(u'消息').click()
         self.dd.find_element_by_name(u'我的').click()
+        swipeDown(5)
         self.dd.find_element_by_name(u'退出登录').click()
 
     def logout(self):
